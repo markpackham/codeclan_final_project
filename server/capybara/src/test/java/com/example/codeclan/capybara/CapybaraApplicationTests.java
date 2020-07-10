588213656bb480f740a92bb0b9a8d6e479378439
mark <markpackham1@gmail.com>
2020-07-10 18:00:30 +0100

    Create seperate Customer Test file

--- server/capybara/src/test/java/com/example/codeclan/capybara/CapybaraApplicationTests.java
+++ server/capybara/src/test/java/com/example/codeclan/capybara/CapybaraApplicationTests.java
@@ -34,11 +34,6 @@ class CapybaraApplicationTests {
 	void contextLoads() {
 	}
 
-	@Test
-	public void createCustomerThenSave(){
-		Customer customer1 = new Customer("Abby", "Anvil", "111111", "abbyanvil@gmail.com");
-		customerRepository.save(customer1);
-	}
 
 	@Test
 	public void createVenueThenSave(){
@@ -87,38 +82,6 @@ class CapybaraApplicationTests {
 		assertNotNull(venueRepository.findById(1L));
 		assertNotNull(venueTableRepository.findById(1L));
 		assertNotNull(reservationRepository.findById(1L));
-	}
-	
-	@Test
-	public void canFindCustomerFirstAndLastName(){
-		Customer customer999 = new Customer("RareFirstName", "RareLastName", "111111", "abbyanvil@gmail.com");
-		customerRepository.save(customer999);
-		List<Customer> foundCustomer = customerRepository.findByFirstNameAndLastName("RareFirstName", "RareLastName");
-		assertEquals(1,foundCustomer.size());
-	}
-
-	@Test
-	public void canFindCustomerLastName(){
-		Customer customer999 = new Customer("Abby", "UniqueLastName", "111111", "abbyanvil@gmail.com");
-		customerRepository.save(customer999);
-		List<Customer> foundCustomer = customerRepository.findByLastName("UniqueLastName");
-		assertEquals(1,foundCustomer.size());
-	}
-
-	@Test
-	public void canFindCustomerEmail(){
-		Customer customer12 = new Customer("FirstName12", "LastName12", "333333", "customer12@gmail.com");
-		customerRepository.save(customer12);
-		Customer foundCustomer = customerRepository.findByEmail("customer12@gmail.com");
-		assertNotNull(foundCustomer);
-	}
-
-	@Test
-	public void canFindCustomerPhone(){
-		Customer customer13 = new Customer("FirstName13", "LastName13", "131313131313", "customer13@gmail.com");
-		customerRepository.save(customer13);
-		Customer foundCustomer = customerRepository.findByPhone("131313131313");
-		assertNotNull(foundCustomer);
 	}
 
 	@Test
