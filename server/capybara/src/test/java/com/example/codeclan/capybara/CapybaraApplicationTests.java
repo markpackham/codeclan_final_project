7c601926c33e511f621869981a24bdb179d28c9d
mark <markpackham1@gmail.com>
2020-07-10 16:05:56 +0100

    Test Can Save Venue Table

--- server/capybara/src/test/java/com/example/codeclan/capybara/CapybaraApplicationTests.java
+++ server/capybara/src/test/java/com/example/codeclan/capybara/CapybaraApplicationTests.java
@@ -1,5 +1,9 @@
 package com.example.codeclan.capybara;
 
+import com.example.codeclan.capybara.models.Customer;
+import com.example.codeclan.capybara.models.Reservation;
+import com.example.codeclan.capybara.models.Venue;
+import com.example.codeclan.capybara.models.VenueTable;
 import com.example.codeclan.capybara.repositories.ICustomerRepository;
 import com.example.codeclan.capybara.repositories.IReservationRepository;
 import com.example.codeclan.capybara.repositories.IVenueRepository;
@@ -7,6 +11,9 @@ import com.example.codeclan.capybara.repositories.IVenueTableRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.time.Month;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -27,8 +34,35 @@ class CapybaraApplicationTests {
 	}
 
 	@Test
-	public void placeholderTest(){
-	assertEquals(1,1);
+	public void createCustomerThenSave(){
+		Customer customer1 = new Customer("Abby", "Anvil", "111111", "abbyanvil@gmail.com");
+		customerRepository.save(customer1);
 	}
+
+	@Test
+	public void createVenueThenSave(){
+		Venue venue1 = new Venue("The Empty Venue");
+		venueRepository.save(venue1);
+	}
+
+	@Test
+	public void createVenueTableThenSave(){
+		Venue venue1 = new Venue("The Empty Venue");
+		venueRepository.save(venue1);
+		VenueTable venueTable1 = new VenueTable(4, venue1);
+		venueTableRepository.save(venueTable1);
+	}
+
+//	@Test
+//	public void createReservationThenSave(){
+//		Customer customer1 = new Customer("Abby", "Anvil", "111111", "abbyanvil@gmail.com");
+//		Venue venue1 = new Venue("The Empty Venue");
+//		VenueTable venueTable1 = new VenueTable(4, venue1);
+//
+//		Reservation reservation1 = new Reservation(customer1, venueTable1,
+//				LocalDateTime.of(2020, Month.AUGUST, 31, 18, 30),
+//				LocalDateTime.of(2020, Month.AUGUST, 31, 20, 30));
+//		reservationRepository.save(reservation1);
+//	}
 
 }
