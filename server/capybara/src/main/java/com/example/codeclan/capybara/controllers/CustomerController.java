61b3c3eb1a491c174928e4b91fa026b0217be726
mark <markpackham1@gmail.com>
2020-07-10 13:03:02 +0100

    Find Email and Phone

--- server/capybara/src/main/java/com/example/codeclan/capybara/controllers/CustomerController.java
+++ server/capybara/src/main/java/com/example/codeclan/capybara/controllers/CustomerController.java
@@ -1,12 +1,11 @@
 package com.example.codeclan.capybara.controllers;
 
+import com.example.codeclan.capybara.models.Customer;
 import com.example.codeclan.capybara.respositories.ICustomerRespository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/customers")
@@ -16,8 +15,28 @@ public class CustomerController {
     ICustomerRespository customerRespository;
 
     @GetMapping
-    public ResponseEntity getAllCustomersAndFilters(){
+    public ResponseEntity getAllCustomersAndFilters(
+            @RequestParam(required = false, name = "email") String email,
+            @RequestParam(required = false, name = "phone") String phone
+    ){
+
+        // url path, http://localhost:8080/customers?email=abbyanvil@gmail.com
+        if(email != null){
+            return new ResponseEntity(customerRespository.findByEmail(email), HttpStatus.OK);
+        }
+
+        // url path, http://localhost:8080/customers?phone=111111
+        if(phone != null){
+            return new ResponseEntity(customerRespository.findByPhone(phone), HttpStatus.OK);
+        }
+
         return new ResponseEntity(customerRespository.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Customer>createCustomer(@RequestBody Customer customer){
+        customerRespository.save(customer);
+        return new ResponseEntity<>(customer,HttpStatus.CREATED);
     }
 
 }
--- server/capybara/src/main/java/com/example/codeclan/capybara/respositories/ICustomerRespository.java
+++ server/capybara/src/main/java/com/example/codeclan/capybara/respositories/ICustomerRespository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ICustomerRespository extends JpaRepository<Customer, Long> {
+    Customer findByEmail(String email);
+
+    Customer findByPhone(String phone);
 }
