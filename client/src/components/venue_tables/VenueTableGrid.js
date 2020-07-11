116e6622fb8397be8f388727ac581bb58fab0aa9
idmacleod <idmacleod@gmail.com>
2020-07-11 15:15:45 +0100

    Passing props down through the component hierarchy

--- client/src/components/venue_tables/VenueTableGrid.js
+++ client/src/components/venue_tables/VenueTableGrid.js
@@ -2,8 +2,15 @@ import React from 'react'
 import VenueTable from './VenueTable';
 
 const VenueTableGrid = (props) => {
+    const venueTableNodes = props.venueTables.map((venueTable, index) => {
+        return <VenueTable key={index} venueTable={venueTable} />
+    });
+
     return (
-        <VenueTable />
+        <div className="venue-table-grid">
+            <h2>All Tables</h2>
+            {venueTableNodes}
+        </div>
     );
 }
 
