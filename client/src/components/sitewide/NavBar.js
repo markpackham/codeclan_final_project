00b04285adfbe616e9bb72c3633946bb152fb22c
Cameron <Cameron-tait@hotmail.com>
2020-07-12 20:13:54 +0100

    Added simple navbar

---
+++ client/src/components/sitewide/NavBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavBar = () => (
+  <ul>
+    <li>
+      <Link to="/">Home</Link>
+    </li>
+    <li>
+      <Link to="/about">About</Link>
+    </li>
+    <li>
+      <Link to="/ReservationForm">NEW RESERVATION</Link>
+    </li>
+  </ul>
+);
+
+export default NavBar;
