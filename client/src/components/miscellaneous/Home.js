23681c15549215ae6fddb36c5bd2a024d2c748f8
Cameron <Cameron-tait@hotmail.com>
2020-07-12 20:43:59 +0100

    Filled in home.js

--- client/src/components/miscellaneous/Home.js
+++ client/src/components/miscellaneous/Home.js
@@ -1,1 +1,11 @@
-import React, { Component } from 'react';
+import React from "react";
+
+const Home = () => (
+  <div>
+      <br></br>
+    <h4>Home</h4>
+    <p>Welcome to Capybara Cafe's homepage!</p>
+  </div>
+);
+
+export default Home;
