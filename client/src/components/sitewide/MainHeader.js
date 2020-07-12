6174ef807c466f5d60c30518e55bc0e6a19aebf6
Cameron <Cameron-tait@hotmail.com>
2020-07-12 20:20:15 +0100

    Added a capybara image as placeholder for header

---
+++ client/src/components/sitewide/MainHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import CapybaraImage from './capybaraimage.png';
+
+const MainHeader = () => (
+    <div className="main-header">
+        <img src={CapybaraImage} width="90" height="86" alt="Capybara!"/>
+
+        </div>
+  );
+
+export default MainHeader;
