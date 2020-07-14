b68922d0e51cf8c2b3c8ead2b6fe9270b045a77d
mark <markpackham1@gmail.com>
2020-07-14 18:55:11 +0100

    Revert "Add reservation notes to form"
    This reverts commit 21c67e013980c8e5c3e5f8d565c0a276f3068fa5.

--- client/src/components/reservations/ReservationForm.js
+++ client/src/components/reservations/ReservationForm.js
@@ -1,258 +1,218 @@
-import React, { Component } from "react";
-import "../../styles/ReservationForm.css";
-import CustomerForm from "../customers/CustomerForm";
-import moment from "moment";
+import React, { Component } from 'react'
+import '../../styles/ReservationForm.css'
+import CustomerForm from '../customers/CustomerForm';
+import moment from 'moment';
+
 
 class ReservationForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      customer: "",
-      venueTable: "",
-      start: "",
-      end: "",
-      partySize: 1,
-      duration: 2,
-      reservationNotes: "",
-      availableTables: [],
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            customer: "",
+            venueTable: "",
+            start: "",
+            end: "",
+            partySize: 1,
+            duration: 2,
+            availableTables: []
+        };
 
-    this.handleStartChange = this.handleStartChange.bind(this);
-    this.handleDurationChange = this.handleDurationChange.bind(this);
-    this.handlePartySizeChange = this.handlePartySizeChange.bind(this);
-    this.handleReservationNotesChange = this.handleReservationNotesChange.bind(
-      this
-    );
-    this.handleCustomerSelect = this.handleCustomerSelect.bind(this);
-    this.handleVenueTableSelect = this.handleVenueTableSelect.bind(this);
-    this.selectCustomerById = this.selectCustomerById.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.updateEnd = this.updateEnd.bind(this);
-  }
-
-  handleStartChange(event) {
-    this.setState(
-      {
-        start: event.target.value,
-      },
-      () => this.updateEnd(),
-      () => this.updateAvailableTables()
-    );
-  }
-
-  handleDurationChange(event) {
-    this.setState(
-      {
-        duration: event.target.value,
-      },
-      () => this.updateEnd()
-    );
-  }
-
-  handlePartySizeChange(event) {
-    this.setState(
-      {
-        partySize: event.target.value,
-      },
-      () => this.updateAvailableTables()
-    );
-  }
-
-  handleReservationNotesChange(event) {
-    this.setState(
-      {
-        reservationNotes: event.target.value,
-      },
-      () => this.updateAvailableTables()
-    );
-  }
-
-  handleCustomerSelect(event) {
-    this.setState({
-      customer: event.target.value,
-    });
-  }
-
-  handleVenueTableSelect(event) {
-    this.setState({
-      venueTable: event.target.value,
-    });
-  }
-
-  selectCustomerById(id) {
-    this.setState({
-      customer: id,
-    });
-  }
-
-  addReservation() {
-    const url = "http://localhost:8080/reservations";
-    const newReservation = {
-      customer: { id: this.state.customer },
-      venueTable: { id: this.state.venueTable },
-      start: this.state.start,
-      end: this.state.end,
-      partySize: this.state.partySize,
-      reservationNotes: this.state.reservationNotes,
-    };
-    return fetch(url, {
-      method: "POST",
-      body: JSON.stringify(newReservation),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then((json) => this.props.onReservationSubmit(json));
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    this.addReservation();
-    this.setState({
-      customer: "",
-      venueTable: "",
-      start: "",
-      end: "",
-      partySize: 1,
-      reservationNotes: "",
-    });
-  }
-
-  updateEnd() {
-    const startMoment = moment(this.state.start);
-    const endMoment = startMoment.add(this.state.duration, "hours");
-    const newEnd = endMoment.format().slice(0, 16);
-    this.setState({
-      end: newEnd,
-    });
-  }
-
-  updateAvailableTables() {
-    const availableTables = [...this.props.venueTables];
-    const newAvailableTables = availableTables.filter((table) => {
-      return (
-        table.covers >= this.state.partySize && this.checkTableAvailable(table)
-      );
-    });
-    this.setState({
-      availableTables: newAvailableTables,
-    });
-  }
-
-  checkTimeAvailable(reservation) {
-    const startMoment = moment(this.state.start);
-    const endMoment = moment(this.state.end);
-    const reservationStart = moment(reservation.start);
-    const reservationEnd = moment(reservation.end);
-    if (reservationStart.isBetween(startMoment, endMoment)) {
-      return false;
+        this.handleStartChange = this.handleStartChange.bind(this);
+        this.handleDurationChange = this.handleDurationChange.bind(this);
+        this.handlePartySizeChange = this.handlePartySizeChange.bind(this);
+        this.handleCustomerSelect = this.handleCustomerSelect.bind(this);
+        this.handleVenueTableSelect = this.handleVenueTableSelect.bind(this);
+        this.selectCustomerById = this.selectCustomerById.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.updateEnd = this.updateEnd.bind(this);
     }
-    if (reservationEnd.isBetween(startMoment, endMoment)) {
-      return false;
-    }
-    return true;
-  }
 
-  checkTableAvailable(venueTable) {
-    const reservations = venueTable.reservations;
-    if (reservations.length === 0) {
-      return true;
+    handleStartChange(event) {     
+        this.setState({
+            start: event.target.value
+        }, () => this.updateEnd(),
+        () => this.updateAvailableTables());;
     }
-    for (const reservation of reservations) {
-      if (this.checkTimeAvailable(reservation)) {
+
+    handleDurationChange(event) {
+        this.setState({
+            duration: event.target.value
+        }, () => this.updateEnd());
+    }
+
+    handlePartySizeChange(event) {
+        this.setState({
+            partySize: event.target.value,
+        }, () => this.updateAvailableTables());
+    }
+    
+    handleCustomerSelect(event) {
+        this.setState({
+            customer: event.target.value
+        });
+    }
+
+    handleVenueTableSelect(event) {
+        this.setState({
+            venueTable: event.target.value
+        });
+    }
+
+    selectCustomerById(id) {
+        this.setState({
+            customer: id
+        });
+    }
+
+    addReservation() {
+        const url = 'http://localhost:8080/reservations';
+        const newReservation = {
+            customer: {id: this.state.customer},
+            venueTable: {id: this.state.venueTable},
+            start: this.state.start,
+            end: this.state.end,
+            partySize: this.state.partySize
+        };
+        return fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(newReservation),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(res => res.json())
+        .then(json => this.props.onReservationSubmit(json));
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        this.addReservation();
+        this.setState({
+            customer: "",
+            venueTable: "",
+            start: "",
+            end: "",
+            partySize: 1
+        });
+    }
+
+    updateEnd() {
+        const startMoment = moment(this.state.start);
+        const endMoment = startMoment.add(this.state.duration, 'hours');
+        const newEnd = endMoment.format().slice(0, 16);
+        this.setState({
+            end: newEnd
+        });
+    }
+
+    updateAvailableTables() {
+        const availableTables = [...this.props.venueTables];
+        const newAvailableTables = availableTables.filter(table => {
+            return (table.covers >= this.state.partySize) && this.checkTableAvailable(table)
+        });
+        this.setState({
+            availableTables: newAvailableTables
+        });
+    }
+
+    checkTimeAvailable(reservation) {
+        const startMoment = moment(this.state.start);
+        const endMoment = moment(this.state.end);
+        const reservationStart = moment(reservation.start);
+        const reservationEnd = moment(reservation.end);
+        if (reservationStart.isBetween(startMoment, endMoment)) {
+            return false;
+        }
+        if (reservationEnd.isBetween(startMoment, endMoment)) {
+            return false;
+        }
         return true;
-      }
     }
-    return false;
-  }
 
-  render() {
-    const customerOptions = this.props.customers.map((customer) => {
-      return (
-        <option key={customer.id} value={customer.id}>
-          {customer.firstName + " " + customer.lastName}
-        </option>
-      );
-    });
+    checkTableAvailable(venueTable) {
+        const reservations = venueTable.reservations;
+        if (reservations.length === 0) {
+            return true;
+        }
+        for (const reservation of reservations) {
+            if (this.checkTimeAvailable(reservation)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-    const venueTableOptions = this.state.availableTables.map((venueTable) => {
-      return (
-        <option key={venueTable.id} value={venueTable.id}>
-          {`Table ${venueTable.id}: Seats ${venueTable.covers}`}
-        </option>
-      );
-    });
+    render() {
+        const customerOptions = this.props.customers.map(customer => {
+            return (
+                <option key={customer.id} value={customer.id}>
+                    {customer.firstName + " " + customer.lastName}
+                </option>
+            );
+        });
 
-    return (
-      <div>
-        <h1>New Customer</h1>
-        <CustomerForm
-          onCustomerSubmit={this.props.onCustomerSubmit}
-          selectCustomerById={this.selectCustomerById}
-        />
-        <h1>New Reservation</h1>
-        <form className="reservation-form" onSubmit={this.handleSubmit}>
-          <label htmlFor="Reservation Date">Reservation Date</label>
-          <input
-            name="Reservation Date"
-            type="datetime-local"
-            value={this.state.start}
-            onChange={this.handleStartChange}
-            required
-          />
+        const venueTableOptions = this.state.availableTables.map(venueTable => {
+            return (
+                <option key={venueTable.id} value={venueTable.id}>
+                    {`Table ${venueTable.id}: Seats ${venueTable.covers}`}
+                </option>
+            );
+        });
 
-          <label htmlFor="Duration">Duration:</label>
-          <input
-            name="Duration"
-            type="number"
-            value={this.state.duration}
-            onChange={this.handleDurationChange}
-            required
-            min="1"
-          />
+        return (
+            <div>
+                <h1>New Customer</h1>
+                <CustomerForm onCustomerSubmit={this.props.onCustomerSubmit} selectCustomerById={this.selectCustomerById} />
+                <h1>New Reservation</h1>
+                <form className="reservation-form" onSubmit={this.handleSubmit}>
+                    <label htmlFor="Reservation Date">Reservation Date</label>
+                    <input
+                        name="Reservation Date"
+                        type="datetime-local" 
+                        value={this.state.start}
+                        onChange={this.handleStartChange}
+                        required
+                    />
 
-          <label htmlFor="Party Size">Party Size:</label>
-          <input
-            name="Party Size"
-            type="number"
-            value={this.state.partySize}
-            onChange={this.handlePartySizeChange}
-            required
-            min="1"
-          />
+                    <label htmlFor="Duration">Duration:</label>
+                    <input
+                        name="Duration"
+                        type="number"
+                        value={this.state.duration}
+                        onChange={this.handleDurationChange}
+                        required
+                        min="1"
+                    />  
 
-          <label htmlFor="Customer">Customer:</label>
-          <select
-            name="Customer"
-            value={this.state.customer}
-            onChange={this.handleCustomerSelect}
-          >
-            {customerOptions}
-          </select>
+                    <label htmlFor="Party Size">Party Size:</label>
+                    <input
+                        name="Party Size"
+                        type="number"
+                        value={this.state.partySize}
+                        onChange={this.handlePartySizeChange}
+                        required
+                        min="1"
+                    />
 
-          <label htmlFor="Venue Table">Table:</label>
-          <select
-            name="Venue Table"
-            value={this.state.venueTable}
-            onChange={this.handleVenueTableSelect}
-          >
-            {venueTableOptions}
-          </select>
+                    <label htmlFor="Customer">Customer:</label>
+                    <select
+                        name="Customer"
+                        value={this.state.customer}
+                        onChange={this.handleCustomerSelect}
+                    >{customerOptions}</select>
 
-          <label htmlFor="Reservation Notes">Reservation Notes:</label>
-          <input
-            name="Reservation Notes"
-            type="text"
-            value={this.state.reservationNotes}
-            onChange={this.handleReservationNotesChange}
-          />
-
-          <input type="submit" value="Create Reservation" />
-        </form>
-      </div>
-    );
-  }
+                    <label htmlFor="Venue Table">Table:</label>
+                    <select
+                        name="Venue Table"
+                        value={this.state.venueTable}
+                        onChange={this.handleVenueTableSelect}
+                    >{venueTableOptions}</select>
+                    
+                    <input type="submit" value="Create Reservation"/>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default ReservationForm;
