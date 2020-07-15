10b533f808f7e89b28b13d4b991f4f8c9bf7a2e5
Paulo Rocha <paulorocha1988@gmail.com>
2020-07-15 14:21:29 +0100

    fix undefined reservation

--- client/src/components/miscellaneous/About.js
+++ client/src/components/miscellaneous/About.js
@@ -3,8 +3,13 @@ import '../../styles/Miscellaneous.css';
 
 const About = () => (
     <div className="about">
-      <h2>About</h2>
-      <p>Here at Capybara Cafe we sell fried capybara, boiled capybara, baked capybara, sauted capybara, grilled capybara, and best of all... stewed capybara!</p>
+      <h2>Welcome to the Capybara Cafe booking app!</h2>
+      <h3>User Instructions:</h3>
+      <p>To add a new customer navigate to the New Reservation page where you can fill in their details.</p>
+      <p>To add a new reservation fill in the required details on the New Reservation page. </p>
+      <p>To view all customers navigate to the All Customers page.</p>
+      <p>On the New Reservation Page the circles represent tables and the number shows how many customers they can seat.</p>
+      <p>On the left hand side of the page there is a calendar to help you keep a track of the days.</p>
     </div>
   );
   
