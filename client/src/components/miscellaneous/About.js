711b10e63ee1128178289020a74fb5bf102da546
mark <markpackham1@gmail.com>
2020-07-15 14:15:49 +0100

    sort out instructions on About

--- client/src/components/miscellaneous/About.js
+++ client/src/components/miscellaneous/About.js
@@ -3,8 +3,15 @@ import '../../styles/Miscellaneous.css';
 
 const About = () => (
     <div className="about">
-      <h2>About</h2>
-      <p>Here at Capybara Cafe we sell fried capybara, boiled capybara, baked capybara, sauted capybara, grilled capybara, and best of all... stewed capybara!</p>
+      <h1>Welcome to the Capybara Cafe booking app!</h1>
+      <h2>User Instructions:</h2>
+      <ul>
+      <li>To add a new customer navigate to the New Reservation page where you can fill in their details.</li>
+      <li>To add a new reservation fill in the required details on the New Reservation page. </li>
+      <li>To view all customers navigate to the All Customers page.</li>
+      <li>On the New Reservation Page the circles represent tables and the number shows how many customers they can seat.</li>
+      <li>On the left hand side of the page there is a calendar to help you keep a track of the days.</li>
+      </ul>
     </div>
   );
   
