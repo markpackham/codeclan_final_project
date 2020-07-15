87e8a825c63079b578c141e10bcb65319c63321b
Cameron <Cameron-tait@hotmail.com>
2020-07-15 15:45:54 +0100

    added image and changed text and allignment on 404 page

--- client/src/components/miscellaneous/ErrorPage.js
+++ client/src/components/miscellaneous/ErrorPage.js
@@ -1,9 +1,11 @@
 import React from "react";
 import '../../styles/Miscellaneous.css';
+import CapybaraImage from './capybaraimage.png';
 
 const ErrorPage = ()=> (
   <div className="error-page">
-  <h1>404 - PAGE NOT FOUND</h1>
+  <h1>404 - CAPYBARA NOT FOUND</h1>
+  <img src={CapybaraImage} width="35%" height="35%" alt="Capybara!" class="center"/>
   </div>
 )
 
