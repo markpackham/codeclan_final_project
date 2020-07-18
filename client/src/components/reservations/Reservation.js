b60a0d1f26603866c1ef57f8812f023f8015506f
mark <markpackham1@gmail.com>
2020-07-18 10:36:59 +0100

    can log out reservation id

--- client/src/components/reservations/Reservation.js
+++ client/src/components/reservations/Reservation.js
@@ -3,6 +3,17 @@ import '../../styles/ReservationDetail.css';
 import moment from 'moment';
 
 class Reservation extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.deleteReservation = this.deleteReservation.bind();
+    }
+
+    deleteReservation(id){
+        console.log(id);
+    }
+
     render() {
         const start = moment(this.props.reservation.start).format("HH:mm");
         const end = moment(this.props.reservation.end).format("HH:mm");
@@ -14,9 +25,11 @@ class Reservation extends Component {
                     <li>{this.props.reservation.customer.firstName} {this.props.reservation.customer.lastName}</li>
                     <li>{this.props.reservation.partySize} Guest(s) / Table {this.props.reservation.venueTable.id}</li>
                     {this.props.reservation.reservationNotes && <li className="italics">Notes: {this.props.reservation.reservationNotes}</li>}
+                    <button onClick={() => this.deleteReservation(this.props.reservation.id)} className="btn-delete">Delete</button>
                 </ul>
             </div>
         )
+
     }
 }
 
